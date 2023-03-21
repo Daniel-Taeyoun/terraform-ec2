4799cdf9dc4ccf05c6641beb0b3f7a908def54ce
Daniel_TaeyoonNam <daniel.nam@themedium.io>
2023-03-21 14:16:15 +0900

    refact(1-Network) : 하드코딩 수정

--- 1-Network/main.tf
+++ 1-Network/main.tf
@@ -1,4 +1,4 @@
-variable "demo_devops_region" {
+variable "aws_region" {
   type = map(string)
   default = {
     "develop" : "ap-northeast-2"
@@ -7,8 +7,49 @@ variable "demo_devops_region" {
   }
 }
 
+variable "environment" {
+  type = map(string)
+  default = {
+    "develop" : "dev"
+    "stage" : "stg"
+    "main" : "prd"
+  }
+}
+
+variable "cidr_block" {
+  type        = map(string)
+  description = "VPN CIDR value"
+  default     = {
+    "develop" : "10.12.0.0/16"
+  }
+}
+
+variable "public_subnet_cidr" {
+  type        = map(list(string))
+  description = "Public Subnet CIDR values"
+  default     = {
+    "develop" : ["10.12.0.0/24", "10.12.8.0/24"]
+  }
+}
+
+variable "private_subnet_cidr" {
+  type        = map(list(string))
+  description = "Private Subnet CIDR values"
+  default     = {
+    "develop" : ["10.12.24.0/24", "10.12.32.0/24"]
+  }
+}
+
+variable "azs" {
+  type        = map(list(string))
+  description = "Availability Zone"
+  default     = {
+    "develop" : ["ap-northeast-2a", "ap-northeast-2c"]
+  }
+}
+
 provider "aws" {
-  region = var.demo_devops_region[terraform.workspace]
+  region = var.aws_region[terraform.workspace]
   profile = "default"
 }
 
@@ -17,4 +58,16 @@ provider "aws" {
 #########################################
 module "aws_network" {
   source = "../modules/network"
+  aws_region = var.aws_region[terraform.workspace]
+  service_name = "tch-devops"
+
+  cidr_block = var.cidr_block[terraform.workspace]
+  public_subnet_cidr = var.public_subnet_cidr[terraform.workspace]
+  private_subnet_cidr = var.private_subnet_cidr[terraform.workspace]
+  azs = var.azs[terraform.workspace]
+
+  environment_upper = upper(var.environment[terraform.workspace])
+  environment_lower = lower(var.environment[terraform.workspace])
+
+
 }
