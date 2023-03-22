f6350f835e9ea8903e4e1eeae3f075e20d4cf5dd
Daniel_TaeyoonNam <daniel.nam@themedium.io>
2023-03-22 16:57:22 +0900

    feat(2-EC2) : data 활용. vpc-id, subnet-id 하드코딩 제거

--- 2-EC2/main.tf
+++ 2-EC2/main.tf
@@ -1,3 +1,9 @@
+locals {
+  service_name      = "tch-devops"
+  subnets_private_a = "sbn-dev-an2-tch-devops-private-a"
+  subnets_private_c = "sbn-dev-an2-tch-devops-private-c"
+}
+
 variable "aws_region" {
   type = map(string)
   default = {
@@ -6,13 +12,38 @@ variable "aws_region" {
     "main" : "ap-northeast-1"
   }
 }
-variable "vpc_id" {
-  default = "vpc-0835e68a89fedca6a"
+
+variable "environment" {
+  type = map(string)
+  default = {
+    "develop" : "dev"
+    "stage" : "stg"
+    "main" : "prd"
+  }
 }
 
-variable "subnet_id" {
-  type = list(string)
-  default = ["subnet-04877697587e94e41", "subnet-0b9bfefbadbe30063"]
+## TODO : dev 하드코딩 제거 필요
+data "aws_vpc" "tch_devops_vpc" {
+  tags = {
+    Name = "vpc-dev-${local.service_name}"
+  }
+}
+
+## TODO : subnet ID를 filter?를 통해 List 형태로 호출하는 방법 필요
+data "aws_subnet" "tch_devops_subnets_private_a" {
+  vpc_id = data.aws_vpc.tch_devops_vpc.id
+  filter {
+    name   = "tag:Name"
+    values = [local.subnets_private_a]
+  }
+}
+
+data "aws_subnet" "tch_devops_subnets_private_c" {
+  vpc_id = data.aws_vpc.tch_devops_vpc.id
+  filter {
+    name   = "tag:Name"
+    values = [local.subnets_private_c]
+  }
 }
 
 provider "aws" {
@@ -25,4 +56,18 @@ provider "aws" {
 #######################################################
 module "aws_instance" {
   source = "../modules/instance"
+  vpc_id = data.aws_vpc.tch_devops_vpc.id
+  subnet_id = [data.aws_subnet.tch_devops_subnets_private_a.id, data.aws_subnet.tch_devops_subnets_private_c.id]
+  service_name = local.service_name
+}
+
+terraform {
+  backend "s3" {
+    bucket = "tch-devops-terraform-state"
+    key = "2-EC2/terraform.tfstate"
+    region = "ap-northeast-2"
+
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt = true
+  }
 }
